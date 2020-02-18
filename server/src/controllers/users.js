aa74151f400fad2e2d41d64b846edfae1a79cde4
r-jeeva <r-jeeva@mithrai.com>
2020-02-18 16:25:52 +0530

    logout api changes

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -10,3 +10,37 @@ module.exports.me = async function (req, res) {
     }
   })
 }
+
+module.exports.logout = async function (req, res) {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+        return token.token != req.token
+    })
+    await req.user.save()
+    res.send({
+      status: 200,
+      message: 'Logout successfully'
+    })
+  } catch (error) {
+    res.send({
+      status: 500,
+      message: 'Internal server error'
+    })
+  }
+}
+
+module.exports.logoutAll = async function (req, res) {
+  try {
+    req.user.tokens.splice(0, req.user.tokens.length)
+    await req.user.save()
+    res.send({
+      status: 200,
+      message: 'Logout successfully'
+    })
+  } catch (error) {
+    res.send({
+      status: 500,
+      message: 'Internal server error'
+    })
+  }
+}
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const auth = require('../middlewares/auth')
-const { me } = require('../controllers/users')
+const { me, logout, logoutAll } = require('../controllers/users')
 
 router.use(auth)
 router.get('/me', me)
+router.post('/logout', logout)
+router.post('/logout-all', logoutAll)
 
 module.exports = router
